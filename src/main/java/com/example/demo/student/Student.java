b8a921b66c111796882a7510e7f47246696ed33c
denizerbay <denizerbayy@gmail.com>
2022-07-19 23:14:50 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,51 @@
+package com.example.demo.student;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_generator",
+            sequenceName = "student_generator",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            generator = "student_generator",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long id;
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String email;
+
+
+
+
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    public Student(String name, String email, Gender gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+}
